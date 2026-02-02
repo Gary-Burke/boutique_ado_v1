0b9962e09cd8143df39b6f881c24e66ec9bf0d5a
gary-burke <garylewisburke@gmail.com>
2026-02-02 19:33:14 +0100

    Add sizes to product model and templates

--- bag/views.py
+++ bag/views.py
@@ -12,9 +12,13 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    print(f"item_id: {item_id}")
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
+
+    print(f"request.POST: {request.POST}")
+    print(f"bag: {bag}")
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
@@ -23,4 +27,5 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
 
+    print(f"bag: {bag}")
     return redirect(redirect_url)
